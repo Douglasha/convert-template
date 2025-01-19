5491d92c60f9aa91d6232aa17ff83e652ff55a8e
Douglas <douglas.henriqueamaral@gmail.com>
2025-01-19 15:58:14 -0300

    Manipulando o input amount para receber somente números

--- scripst.js
+++ scripst.js
@@ -1,1 +1,7 @@
-console.log("Javascript carregado!")
+const amount = document.getElementById("amount")
+
+// Manipulando o input amount para receber somente números
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
