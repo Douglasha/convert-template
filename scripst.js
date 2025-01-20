62ce500f7766815900a6b8a83963510b643ce9f0
Douglas <douglas.henriqueamaral@gmail.com>
2025-01-19 21:35:01 -0300

    Exibindo a cotação da moeda

--- scripst.js
+++ scripst.js
@@ -1,7 +1,13 @@
+// Cotação de moedas do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
 // Obtendo os elementos do formulário
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input", () => {
@@ -9,10 +15,34 @@ amount.addEventListener("input", () => {
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
 
-// Capturando o envento de submit do formulário
+// Capturando o evento de submit do formulário
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log(currency.value)
-
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break    
+  }
+}
+//Função para converter a moeda
+function convertCurrency(amount, price, symbol){
+  try {
+    //Aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result")
+  } catch (error) {
+    
+    // Remove a classe do footer removendo ele da tela
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde.")
+    
+  }
 }
